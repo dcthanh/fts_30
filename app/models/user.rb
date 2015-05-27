a09b7ffbd04c34de7c2e9629e613b7c9c8885aae
Nguyen Cuong <cuongnx2603@gmail.com>
2015-05-27 16:27:22 +0700

    User roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :exams, dependent: :destroy
 
+  enum access_level: [:normal, :admin]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+        
 end
